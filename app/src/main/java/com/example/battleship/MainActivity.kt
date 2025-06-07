9bd5f923350f4fb0f73750cdac996c0fea182a9d
CarmineRaduazzo <carmine.raduazzo001@studenti.uniparthenope.it>
2025-06-07 09:48:36 +0200

    Aggiunta pulsante "Play"

--- app/src/main/java/com/example/battleship/MainActivity.kt
+++ app/src/main/java/com/example/battleship/MainActivity.kt
@@ -1,32 +1,41 @@
 package com.example.battleship
+
+//Librerie di sistema
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+
+//JP per UI
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+//Per navigazione
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+
+//Per tema personalizzato
 import com.example.battleship.ui.theme.BattleShipTheme
 
+//Per componenti UI
 import androidx.compose.material3.Text
 
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.stringResource
+//Per font e testo
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.style.TextAlign
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -96,9 +105,36 @@ class MainActivity : ComponentActivity() {
                                             modifier = Modifier.align(Alignment.Center)
                                         )
                                     }
+                                    Box(
+                                        modifier = Modifier
+                                            .width(150.dp)
+                                            .height(60.dp)
+                                            .clickable { navController.navigate("preparazione") }
+                                    ) {
+                                        Image(
+                                            painter = painterResource(id = R.drawable.cyan_button),
+                                            contentDescription = "Play Button",
+                                            contentScale = ContentScale.Inside,
+                                            modifier = Modifier.fillMaxSize()
+                                        )
+                                        Text(
+                                            text = stringResource(id = R.string.play),
+                                            color = Color.Black,
+                                            fontSize = 20.sp,
+                                            fontFamily = customFont,
+                                            modifier = Modifier.align(Alignment.Center)
+                                        )
+                                    }
                                 }
                             }
                         }
+                    }
+                    composable("preparazione") {
+                        GiocoScreen(navController)
+                    }
+
+                    composable("giocoAttivo") {
+                        GiocoAttivoScreen(navController)
                     }
                 }
             }
