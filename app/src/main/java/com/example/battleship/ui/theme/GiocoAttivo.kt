be0afc64ac673f966c14836ca2cf8cbcc26a35f2
Mike9790 <michelemaio597@gmail.com>
2025-06-13 17:10:43 +0200

    Struttura iniziale

---
+++ app/src/main/java/com/example/battleship/ui/theme/GiocoAttivo.kt
@@ -0,0 +1,79 @@
+package com.example.battaglianavale
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.navigation.NavController
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.Text
+import androidx.compose.ui.unit.dp
+
+
+enum class Turno { GIOCATORE, PC }
+
+@Composable
+
+//Recupero delle navi del giocatore e del PC da Gioco.kt
+fun GiocoAttivoScreen(navController: NavController) {
+    val giocatoreShips = navController.previousBackStackEntry
+        ?.savedStateHandle
+        ?.get<List<List<Pair<Int, Int>>>>("giocatoreShips")
+
+    val pcShips = navController.previousBackStackEntry
+        ?.savedStateHandle
+        ?.get<List<List<Pair<Int, Int>>>>("pcShips")
+
+    val turno by remember { mutableStateOf(Turno.GIOCATORE) }
+
+    //Breve check per la presenza di dati: se null mostra errore
+    if (giocatoreShips == null || pcShips == null) {
+        Text("Dati non disponibuli", color = MaterialTheme.colorScheme.error)
+        return
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Spacer(Modifier.height(30.dp)) //Da verificare e approvare
+
+            val snackbarHostState = remember { SnackbarHostState() }
+
+            var messaggioNaveDistrutta by remember { mutableStateOf<String?>(null) }
+
+            if (messaggioNaveDistrutta != null) {
+                SnackbarHost(
+                    hostState = snackbarHostState,
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Snackbar(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        contentColor = MaterialTheme.colorScheme.onPrimary
+                    ) {
+                        Text(messaggioNaveDistrutta!!)
+                    }
+                }
+
+                //Il messaggio "scompare" dopo un po' di tempo
+                LaunchedEffect(messaggioNaveDistrutta) {
+                    snackbarHostState.showSnackbar(
+                        messaggioNaveDistrutta!!,
+                        duration = SnackbarDuration.Short
+                    )
+                    messaggioNaveDistrutta = null
+                }
+            }
+        }
+    }
+}
